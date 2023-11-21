24f8a39e8c9deda8b04b04e4c01873a08377a50e
Aksh-007 <akshaybarapatre54@gmail.com>
2023-11-21 11:17:21 +0530

    Feat - protected route created

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,22 @@
 import { Routes, Route } from "react-router-dom";
 import Home from "./pages/Home.jsx";
+import Login from "./pages/Login.jsx";
+import Profile from "./pages/Profile.jsx";
+import Register from "./pages/Register.jsx";
+import ResetPassword from "./pages/ResetPassword.jsx";
+import ProtectedRoute from "./components/ProtectedRoute.jsx";
 function App() {
+  const isAuthenticated = true;
   return (
     <>
       <Routes>
-        <Route path="/" element={<Home />} />
+        <Route element={<ProtectedRoute isAuthenticated={isAuthenticated} />}>
+          <Route path="/" element={<Home />} />
+          <Route path="/profile/:id?" element={<Profile />} />
+        </Route>
         <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/reset-password" element={<ResetPassword />} />
       </Routes>
     </>
   );
