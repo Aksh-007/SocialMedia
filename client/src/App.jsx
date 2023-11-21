a8b005e6b64d8de8d86a97d24047f1fb43ca25d4
Aksh-007 <akshaybarapatre54@gmail.com>
2023-11-21 11:05:51 +0530

    Feat -Pages created

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,12 @@
-
-
+import { Routes, Route } from "react-router-dom";
+import Home from "./pages/Home.jsx";
 function App() {
   return (
     <>
-     
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+      </Routes>
     </>
   );
 }
