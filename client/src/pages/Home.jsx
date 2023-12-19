4b99c0283661faa12b35835f893fabb2d2ba1d6a
Aksh-007 <akshaybarapatre54@gmail.com>
2023-12-19 09:14:01 +0530

    Feat - added logout functionality

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,9 +1,17 @@
 import React from "react";
+import { useNavigate } from "react-router-dom";
 
 const Home = () => {
+  const navigate = useNavigate();
+  const logout = () => {
+    console.log("logout click");
+    localStorage.removeItem("token");
+    navigate("/login");
+  };
   return (
     <div>
       <h1>Home</h1>
+      <button onClick={logout}>Logout</button>
     </div>
   );
 };
