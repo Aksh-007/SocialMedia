e1d2c6635d8e3820732f589a10bb58296307f519
Aksh-007 <akshaybarapatre54@gmail.com>
2023-12-29 12:38:53 +0530

    Feat - Theme toggle functionaliy added and topbar created

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,17 +1,28 @@
 import React from "react";
 import { useNavigate } from "react-router-dom";
+import { useSelector } from "react-redux";
+import TopBar from "../components/TopBar";
 
 const Home = () => {
+  const { user } = useSelector((state) => state.user);
+
   const navigate = useNavigate();
   const logout = () => {
     console.log("logout click");
     localStorage.removeItem("token");
     navigate("/login");
   };
+
   return (
-    <div>
-      <h1>Home</h1>
-      <button onClick={logout}>Logout</button>
+    <div className="w-full px-0 lg:px-10 pb-20 2xl:px-40 bg-bgColor lg:rounded-lg h-screen overflow-hidden">
+      <TopBar />
+      <h1 className="text-sm font-semibold  mt-2 text-ascent-2">Home</h1>
+      <button
+        className="text-sm font-semibold  mt-2 text-ascent-2"
+        onClick={logout}
+      >
+        Logout
+      </button>
     </div>
   );
 };
