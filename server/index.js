5e8dce5c50241da9549b27f64e7d48810056d8c4
Aksh-007 <akshaybarapatre54@gmail.com>
2024-01-02 15:00:02 +0530

    Feat - added post schema

--- server/index.js
+++ server/index.js
@@ -11,8 +11,19 @@ import helmet from "helmet";
 const app = express();
 
 dotenv.config()
+const PORT = process.env.PORT || 5000;
 dbConnection();
-const PORT = process.env.PORT
+
+// middleware
+app.use(helmet())
+app.use(cors())
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: true }))
+app.use(express.json({ limit: "10mb" }))
+app.use(express.urlencoded({ extended: true }))
+
+app.use(morgan('dev'))
+
 app.get("/", (req, res) => {
     res.send(`<h1>App is Running</h1>`)
 })
---
+++ server/models/post.schema.js
@@ -0,0 +1,29 @@
+
+import mongoose, { Schema } from "mongoose";
+
+
+const postSchema = new mongoose.Schema(
+    {
+        userId: { type: Schema.Types.ObjectId, ref: "User" },
+        description: {
+            type: String,
+            required: [true, "description is required"],
+            trim: true
+        },
+        image: {
+            type: String,
+        },
+        likes: [
+            { type: String }
+        ],
+        comments: [
+            {
+                type: Schema.Types.ObjectId,
+                ref: "Comments"
+            }
+        ],
+    },
+    { timestamps: true }
+)
+
+export default mongoose.model("Post", postSchema);
