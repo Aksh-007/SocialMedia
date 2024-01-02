83e762045c5bc9bcfcc4015ba61f7aaad0de6b76
Aksh-007 <akshaybarapatre54@gmail.com>
2024-01-02 12:55:54 +0530

    Feat - basic backend setup done

---
+++ server/index.js
@@ -0,0 +1,28 @@
+import express from "express";
+import dotenv from "dotenv"
+import cors from "cors";
+import morgan from "morgan"
+import bodyParser from "body-parser";
+// for security
+import helmet from "helmet";
+
+
+const app = express();
+
+dotenv.config()
+const PORT = process.env.PORT
+app.get("/", (req, res) => {
+    res.send(`<h1>App is Running</h1>`)
+})
+
+
+app.listen(PORT, (req, res) => {
+    console.log(`App is listening on http://localhost:${PORT}/`)
+})
+
+
+
+export default app;
+
+
+
