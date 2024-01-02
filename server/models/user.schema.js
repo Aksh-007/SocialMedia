236188b61f2d4442738d657d5f167e16d2d082f5
Aksh-007 <akshaybarapatre54@gmail.com>
2024-01-02 13:15:05 +0530

    Feat - folder structure and user schema added

---
+++ server/models/user.schema.js
@@ -0,0 +1,48 @@
+import mongoose, { Schema } from "mongoose"
+
+const userSchema = new mongoose.Schema(
+    {
+        firstName: {
+            type: String,
+            required: [true, "First Name is Required"],
+            trim: true,
+        },
+        lastName: {
+            type: String,
+            required: [true, "Last Name is Required"],
+            trim: true,
+        },
+        email: {
+            type: String,
+            required: [true, "Email is Required"],
+            unique: true,
+            trim: true
+        },
+        passwords: {
+            type: String,
+            required: [true, "Passwords is Required"],
+            minLength: [6, "Password should be atleast 6 digit"],
+            select: false,
+        },
+        location: { type: String },
+        profileUrl: { type: String },
+        profession: { type: String },
+        friends: [
+            {
+                type: Schema.Types.ObjectId,
+                ref: 'User'
+            }
+        ],
+        views: [
+            { type: String }
+        ],
+        verified: {
+            type: Boolean,
+            default: false,
+        }
+    },
+    { timestamps: true }
+)
+
+
+export default mongoose.model("User", userSchema)
