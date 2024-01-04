e03de86ba0fa29d165c77e8a860cb9bc5a798367
Aksh-007 <akshaybarapatre54@gmail.com>
2024-01-04 10:42:14 +0530

    Fix - small syntax fix

--- server/utility/asyncHandler.js
+++ server/utility/asyncHandler.js
@@ -1,8 +1,8 @@
 
 
-const asyncHandler = (fn) = async (req, res, next) => {
+const asyncHandler = (fn) => async (req, res, next) => {
     try {
-
+        await fn(req, res, next);
     } catch (error) {
         console.log(error)
         res.status(error.code || 500).json({
