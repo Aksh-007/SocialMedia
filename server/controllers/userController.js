0df2ff4006381137f99c21ae4ae5cfa9e7426c01
Aksh-007 <akshaybarapatre54@gmail.com>
2024-01-07 19:52:18 +0530

    Feat - created api for forgot and reset password with routing

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -5,17 +5,17 @@ import randomStringGenerator from "../utility/randomStringGenerator.js"
 
 /******************************************************
  * @GET_VERIFYEMAIL
- * @route http://localhost:5000/api/v1/user/verify/:token
- * @description Verify the email
- * @parameters email , token  
+ * @route http://localhost:5000/api/v1/user/verifyEmail/:userId/:token
+ * @description Verify the userId
+ * @parameters userId , token  
  * @returns Verified User
  ******************************************************/
 export const verifyEmail = asyncHandler(async (req, res) => {
-    const { email } = req.body
-    const { token } = req.params
+    const { token, userId } = req.params
 
-    // console.log("Email verification token", token)
-    const userExists = await userSchema.findOne({ email });
+    console.log("Email verification token", token)
+    console.log("User ID", userId)
+    const userExists = await userSchema.findById(userId)
 
     if (!userExists) throw new Error('No such User Exist please register')
 
@@ -62,10 +62,52 @@ export const forgotPassword = asyncHandler(async (req, res) => {
 
     // setting forgotPasswordToken to undefined before sending response back
     userExists.forgotPasswordToken = undefined
-    // sent 
+    // creating url to sent on mail
+    // http://localhost:5000/api/v1/user/reset-password/:userId/:resetToken
+    const forgotPasswordUrl =
+        `${req.protocol}://${req.get("host")}/api/v1/user/reset-password/${userExists._id}/${resetToken}`
+
     res.status(200).json({
         success: true,
         message: `Password Reset Token sent to:${userExists.email}`,
+        userExists,
+        forgotPasswordUrl
+    })
+})
+
+
+/******************************************************
+ * @POST_PASSWORD_RESET
+ * @route http://localhost:5000/api/v1/user/reset-password/:userId/:resetToken
+ * @description Verify the email,
+ * @parameters email , confirmPassword 
+ * @returns sucess message email sent
+ ******************************************************/
+export const resetPassword = asyncHandler(async (req, res) => {
+    const { resetToken, userId } = req.params;
+    const { password, confirmPassword } = req.body
+
+    if (password !== confirmPassword) throw new Error("Password and confirm password not Match")
+
+    const userExists = await userSchema.findById(userId);
+
+    // console.log("Entered token ", resetToken)
+    // console.log("Entered userId", userId)
+    // console.log("Db User", userExists);
+    const isPasswordResetTokenMatch = await userExists.comparePasswordResetToken(resetToken);
+
+
+    if (!isPasswordResetTokenMatch) throw new Error("Invalid Password Reset Link")
+
+    userExists.password = password;
+    userExists.forgotPasswordToken = "";
+    await userExists.save({ validateBeforeSave: false })
+
+    userExists.password = ""
+
+    res.status(200).json({
+        success: true,
+        message: "Password Reset Sucesfully",
         userExists,
     })
 })
--- server/routes/user.routes.js
+++ server/routes/user.routes.js
@@ -1,10 +1,11 @@
 import express from "express"
-import { forgotPassword, verifyEmail } from "../controllers/userController.js"
+import { forgotPassword, verifyEmail, resetPassword } from "../controllers/userController.js"
 
 const userRouter = express.Router()
 
-userRouter.get("/verify/:token", verifyEmail)
+userRouter.get("/verifyEmail/:userId/:token", verifyEmail)
 userRouter.post("/forgot-password", forgotPassword)
+userRouter.post("/reset-password/:userId/:resetToken", resetPassword)
 
 
 export default userRouter
