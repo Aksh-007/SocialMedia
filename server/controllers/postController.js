08900af5989c0b9d95f72df0c85f1b5b496d753e
Aksh-007 <akshaybarapatre54@gmail.com>
2024-01-11 16:05:17 +0530

    Feat - added create post api controller and route

--- server/controllers/postController.js
+++ server/controllers/postController.js
@@ -1,12 +1,37 @@
 import postSchema from "../models/post.schema.js"
 import asyncHandler from "../utility/asyncHandler.js"
+import CustomError from "../utility/customError.js"
+import cloudinary from "../utility/imageHelper.js"
 /******************************************************
  * @POST_CREATE_POST
- * @route http://localhost:5000/api/v1/post/createPost
+ * @route http://localhost:5000/api/v1/post/createPost/userId
  * @description create the post for user
  * @parameters descrption ,image and userID
  * @returns created post for user
  ******************************************************/
 export const createPost = asyncHandler(async (req, res) => {
-    
+    const { userId } = req.params;
+    const { description } = req.body;
+    const image = req.file;
+
+    if (!description) throw new CustomError("Please Provide description", 400);
+
+    // uploading image in cloudinary
+    const imageUrl = await cloudinary.uploader.upload(
+        req.file.path
+    );
+    console.log("imageUrl secure_url =", imageUrl.secure_url)
+    const newPost = await postSchema.create({
+        userId,
+        description,
+        image: imageUrl.secure_url
+    })
+
+    res.status(200).json({
+        success: true,
+        message: `Post Created Succesfully`,
+        newPost
+    })
+
 })
+
--- server/routes/post.routes.js
+++ server/routes/post.routes.js
@@ -1,6 +1,8 @@
 import express from "express";
-
+import { createPost } from "../controllers/postController.js";
+import { upload } from "../middleware/multer.js";
 const postRouter = express.Router();
 
+postRouter.post("/createPost/:userId", upload.single("file"), createPost)
 
 export default postRouter;
