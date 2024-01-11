bccfa6a5e3ed82b3efcb5767ba2f1c52014a6f5b
Aksh-007 <akshaybarapatre54@gmail.com>
2024-01-11 22:40:50 +0530

    fix- login populate method added

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -92,7 +92,7 @@ export const login = asyncHandler(async (req, res) => {
     if (!password) throw new CustomError('Please Fill password Field', 400);
 
     // here please select password otherwise no password selected
-    const userExist = await userSchema.findOne({ email }).select("+password")
+    const userExist = await userSchema.findOne({ email }).select("+password").populate('friends', 'firstName lastName email')
 
     if (!userExist) throw new CustomError('Please Register User', 404)
 
