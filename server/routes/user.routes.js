c702969aec8d223e3da0f0d7ea2f51b79aeb9b5d
Aksh-007 <akshaybarapatre54@gmail.com>
2024-01-17 15:45:30 +0530

    Feat - auth middleware added to read token

--- server/routes/user.routes.js
+++ server/routes/user.routes.js
@@ -1,17 +1,18 @@
 import express from "express"
 import { forgotPassword, verifyEmail, resetPassword, changePassword, getUserById, updateUser, suggestFriends, sentfriendRequest, getAllFriendRequest, acceptFriendRequest } from "../controllers/user.controller.js"
+import authMiddleware from "../middleware/auth.middleware.js"
 
 const userRouter = express.Router()
 
 userRouter.get("/verifyEmail/:userId/:token", verifyEmail)
 userRouter.post("/forgot-password", forgotPassword)
-userRouter.post("/reset-password/:userId/:resetToken", resetPassword)
-userRouter.post("/change-password/:userId", changePassword);
-userRouter.get("/getUserById/:currentUserId/:userId", getUserById)
-userRouter.put("/updateUser/:userId", updateUser);
-userRouter.get("/friends-suggestion/:userId", suggestFriends)
-userRouter.post("/friendRequest/:userId/:requestedUserId", sentfriendRequest);
-userRouter.get("/friendRequest/:userId", getAllFriendRequest);
-userRouter.post("/acceptFriendRequest/:userId/:requestedId", acceptFriendRequest);
+userRouter.post("/reset-password/:userId/:resetToken", authMiddleware, resetPassword)
+userRouter.post("/change-password/:userId", authMiddleware, changePassword);
+userRouter.get("/getUserById/:currentUserId/:userId", authMiddleware, getUserById)
+userRouter.put("/updateUser/:userId", authMiddleware, updateUser);
+userRouter.get("/friends-suggestion/:userId", authMiddleware, suggestFriends)
+userRouter.post("/friendRequest/:userId/:requestedUserId", authMiddleware, sentfriendRequest);
+userRouter.get("/friendRequest/:userId", authMiddleware, getAllFriendRequest);
+userRouter.post("/acceptFriendRequest/:userId/:requestedId", authMiddleware, acceptFriendRequest);
 
 export default userRouter
