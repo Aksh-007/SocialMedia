94b59f57258ca5c96a284af3ed25ea1f9835935a
Aksh-007 <akshaybarapatre54@gmail.com>
2024-01-19 21:37:23 +0530

    Fix - cors issue trying to fix

--- server/index.js
+++ server/index.js
@@ -17,10 +17,11 @@ dbConnection();
 
 // middleware
 app.use(helmet())
-app.use(cors({
-    origin: process.env.CORS_ORIGIN,
-    credentials: true
-}))
+// app.use(cors({
+//     origin: process.env.CORS_ORIGIN,
+//     credentials: true
+// }))
+app.use(cors())
 app.use(cookieParser())
 app.use(express.json({ limit: "10mb" }))
 app.use(express.urlencoded({ extended: true }))
