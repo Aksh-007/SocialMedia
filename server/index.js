f3394555250bc02f98eccf0c15a165776f764130
Aksh-007 <akshaybarapatre54@gmail.com>
2024-01-20 22:20:54 +0530

    Fix - cors issue fix

--- server/index.js
+++ server/index.js
@@ -16,10 +16,10 @@ const PORT = process.env.PORT || 5000;
 dbConnection();
 
 // middleware
-const allowedOrigins = ['http://localhost:5173', 'https://link-leap.vercel.app'];
+// const allowedOrigins = ['http://localhost:5173', 'https://link-leap.vercel.app'];
 
 app.use(cors({
-    origin: allowedOrigins,
+    origin: true,
     credentials: true
 }));
 app.use(helmet())
