9bc91d72917c2adf20e6c15487a116c9aa46c880
Aksh-007 <akshaybarapatre54@gmail.com>
2024-01-20 11:26:22 +0530

    Feat - added Freind Request api with data binding

--- client/src/components/FriendsRequest.jsx
+++ client/src/components/FriendsRequest.jsx
@@ -18,10 +18,11 @@ const FriendsRequest = () => {
       setIsSubmitiing(true);
       const response = await axios.get(
         `https://social-media-backend-hazel.vercel.app/api/v1/user/friendRequest/${userId}`,
+        // `http://localhost:5000/api/v1/user/friendRequest/${userId}`,
         { withCredentials: true }
       );
       console.log("friend Request", response);
-      setFriendRequest(response.data); // Use response.data to update the state
+      setFriendRequest(response?.data?.friendRequest); // Use response.data to update the state
     } catch (error) {
       console.log(error);
       toast.error(error?.response?.data?.message ?? error?.message);
@@ -43,44 +44,49 @@ const FriendsRequest = () => {
             <span>Friend Request</span>
             <span>{friendRequest?.length}</span>
           </div>
-          <div className="w-full flex flex-col gap-4 pt-4">
-            {friendRequest?.map((request) => (
-              <div
-                className="flex items-center justify-between"
-                key={request._id}
-              >
-                <Link
-                  to={`/profile/${request._id}`}
-                  className="w-full flex gap-4 items-center cursor-pointer"
-                >
-                  <img
-                    src={request?.requestFrom?.profileUrl ?? NoProfile}
-                    alt={request?.requestFrom?.firstName}
-                    className="w-10 h-10 object-cover rounded-full"
-                  />
-                  <div className="flex-1">
-                    <p className="text-base font-medium text-ascent-1">
-                      {`${request?.requestFrom?.firstName} ${request?.requestFrom?.lastName}`}
-                    </p>
-                    <span className="text-sm text-ascent-2">
-                      {request?.requestFrom?.profession ?? "No Profession"}
-                    </span>
-                  </div>
-                </Link>
 
-                <div className="flex gap-1 ">
-                  <CustomButton
-                    title="Accept"
-                    containerStyles="bg-[#0444a4] text-xs text-white px-1.5 py-1 rounded-full"
-                  />
-                  <CustomButton
-                    title="Deny"
-                    containerStyles="border border-[#666] text-xs text-ascent-1 px-1.5 py-1 rounded-full"
-                  />
+          {friendRequest.length === 0 ? (
+            <div className="text-ascent-1">No Request</div>
+          ) : (
+            <div className="w-full flex flex-col gap-4 pt-4">
+              {friendRequest?.map((request) => (
+                <div
+                  className="flex items-center justify-between"
+                  key={request._id}
+                >
+                  <Link
+                    to={`/profile/${request._id}`}
+                    className="w-full flex gap-4 items-center cursor-pointer"
+                  >
+                    <img
+                      src={request?.requestFrom?.profileUrl ?? NoProfile}
+                      alt={request?.requestFrom?.firstName}
+                      className="w-10 h-10 object-cover rounded-full"
+                    />
+                    <div className="flex-1">
+                      <p className="text-base font-medium text-ascent-1">
+                        {`${request?.requestFrom?.firstName} ${request?.requestFrom?.lastName}`}
+                      </p>
+                      <span className="text-sm text-ascent-2">
+                        {request?.requestFrom?.profession ?? "No Profession"}
+                      </span>
+                    </div>
+                  </Link>
+
+                  <div className="flex gap-1 ">
+                    <CustomButton
+                      title="Accept"
+                      containerStyles="bg-[#0444a4] text-xs text-white px-1.5 py-1 rounded-full"
+                    />
+                    <CustomButton
+                      title="Deny"
+                      containerStyles="border border-[#666] text-xs text-ascent-1 px-1.5 py-1 rounded-full"
+                    />
+                  </div>
                 </div>
-              </div>
-            ))}
-          </div>
+              ))}
+            </div>
+          )}
         </div>
       )}
     </>
