7c3e70f925e4fe10700780a789fc98bb2833e901
Aksh-007 <akshaybarapatre54@gmail.com>
2024-01-20 11:17:42 +0530

    Fix - CORS bug fixed

--- server/index.js
+++ server/index.js
@@ -21,10 +21,13 @@ app.use(helmet())
 //     origin: process.env.CORS_ORIGIN,
 //     credentials: true
 // }))
-app.use(cors())
 app.use(cookieParser())
 app.use(express.json({ limit: "10mb" }))
 app.use(express.urlencoded({ extended: true }))
+app.use(cors({
+    credentials: true,
+    origin: true,
+}))
 
 app.use(morgan('dev'))
 
