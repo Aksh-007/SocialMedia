21a348e7ce89dc800aefa13b285e6ef2e4feba0c
Aksh-007 <akshaybarapatre54@gmail.com>
2024-11-05 18:13:33 +0530

    Fix- Made some changes

--- server/index.js
+++ server/index.js
@@ -32,7 +32,7 @@ app.use(morgan('dev'))
 
 // router middleware
 app.use("/api/v1/", router)
-app.get("/", (req, res) => {
+app.get("/api/v1/", (req, res) => {
     res.send(`<h1>App is Running</h1>`)
 })
 
